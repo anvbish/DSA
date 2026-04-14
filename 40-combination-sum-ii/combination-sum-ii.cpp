45e0393254f8d15e5e3bd5c2ec3d79d3bf16f0b2
Anvesha Bishnoi <bishnoianvesha19@gmail.com>
2026-04-14 17:36:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (81.72%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void findComb(int idx,int target,vector<int>& candidates,vector<vector<int>>&ans,  vector<int>&ds){
+        if(target==0){
+            ans.push_back(ds);
+            return;
+        }
+        for(int i=idx;i<candidates.size();i++){
+            if(i>idx && candidates[i]==candidates[i-1])continue;
+            if(candidates[i]>target)break;
+
+            ds.push_back(candidates[i]);
+            findComb(i+1,target-candidates[i],candidates,ans,ds);
+            ds.pop_back();
+
+        }
+    }
+
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>>ans;
+        vector<int>ds;
+
+        findComb(0,target,candidates,ans,ds);
+        return ans;
+    }
+};
