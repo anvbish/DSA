a147b9d9c94c27da1c51a10eda99b1a467ee55a6
Anvesha Bishnoi <bishnoianvesha19@gmail.com>
2026-08-08 17:05:16 +0530

    Time: 889 ms (17.76%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+WITH distinctAns AS
+(
+SELECT product_id,year,
+        quantity,price,
+        RANK() OVER (
+               PARTITION BY product_id
+               ORDER BY year
+           ) AS rnk
+        FROM Sales
+)
+SELECT product_id,year AS first_year,quantity,price
+FROM distinctAns
+WHERE rnk=1;
+    
