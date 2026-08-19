20901ea72450953f4af6a6eb21e874bf48085afc
Anvesha Bishnoi <bishnoianvesha19@gmail.com>
2026-08-19 14:38:47 +0530

    Time: 15 ms (71.96%) | Memory: 12.8 MB (59.61%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        lp=0
+        rp=len(s)-1
+
+        while(lp<=rp):
+            if not s[lp].isalnum():
+                lp+=1
+                continue
+            if not s[rp].isalnum():
+                rp-=1
+                continue
+            if s[lp].lower()!=s[rp].lower():
+                return False
+
+            lp+=1
+            rp-=1
+        
+        return True
+        
